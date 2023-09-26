4244cb7fc9cc3c370f322aa34c657e0f14413ad1
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-09-26 20:57:43 +0530

    Add files via upload

---
+++ naturalno.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+    printf("write the programe to print the natural no\n");
+    int n=1;
+   while(n>=1){
+    printf("%d is natural no");
+    n++;
+   }
+   
+    return 0;
+}
