55314c6a5d3392125fc501687673e81234e76aa8
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-09-26 20:58:09 +0530

    Add files via upload

---
+++ swapno.c
@@ -0,0 +1,23 @@
+# include<stdio.h>
+void swap(int a , int b);
+void _swap(int*a , int *b);
+int main(){
+    int x = 6, y = 4;
+    swap(x , y);
+        printf("x = %d & y = %d\n", x , y);
+    
+    _swap(&x , &y);
+        printf("x = %d & y = %d\n", x , y);
+    
+    return 0;
+}
+void swap(int a , int b){
+    int t = a;
+    a = b;
+    b = t;
+}
+void _swap(int *a , int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
