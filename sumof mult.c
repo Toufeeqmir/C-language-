ddc2495af5c3938a63e8bc77b73df319d2f42c8c
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-09-28 12:10:59 +0530

    Add files via upload

---
+++ sumof mult.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    /*int i = 1;
+    int sum = 0, n;
+    printf("Enter the value of number\n");
+    scanf("%d\n", &n);
+    while (i <= 20)
+    {
+        printf("%d *%d = %d\n", n, i, n * i);
+        i++;
+        sum += n * i;
+    }
+    printf("The sum of multiplcation table  is %d\n", sum);
+    return 0;
+}*/
+//int i = 1;
+//int sum = 0 , n;
+//printf("enter the value of number\n");
+//scanf("%d\n",&n);
+//while(i<=10){
+ //   printf("%d * %d = %d\n", n , i , n*i);
+ //   i++;
+ //   sum += n*i;
+//}//
+//printf("the sum of multiplication table  is %d\n", sum);
+
+
+// int a ;
+// printf("enter the value of a :\n");
+// scanf("%d",&a);
+
+int sum = 0 ;
+
+for(int i =1 ;i<10;i++){
+    printf("the sum of ist ten natural numbers is %d\n",sum);
+    sum+=i;
+}
+return 0;
+}
