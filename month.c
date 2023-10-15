2195d83e25379e4522e3d35525f0b215a017f321
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-10-15 21:38:44 +0530

    Add files via upload

---
+++ month.c
@@ -0,0 +1,56 @@
+# include<stdio.h>
+
+int main(){
+    int m;
+    printf("enter the value of m :");
+    scanf("%d",&m);
+    if(m==1){
+        printf("january");
+
+    }
+    else if(m==2){
+        printf("febuary");
+    }
+    else if(m==3){
+        printf("march");
+
+    }
+    else if(m==4)
+    {
+        printf("april");
+    }
+    else if(m==5)
+    {
+        printf("may");
+    }
+    else if(m==6)
+{
+    printf("june");
+
+}
+else if(m==7)
+{
+    printf("july");
+}
+else if(m==8)
+{
+    printf("august");
+
+}
+else if(m==9)
+{
+    printf("september");
+}
+else if(m==10)
+{
+    printf("october");
+}
+else if(m==11)
+{
+    printf("november");
+}
+else{
+    printf("december");
+}
+    return 0;
+}
