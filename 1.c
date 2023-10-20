39173f72a4176adee2a070e6c7660d9359d1dd51
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-10-20 11:44:09 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 int main()
 {
-char name[] = "KINSMAN";
-int i=0;
-while(i<=7){
-    printf("%c",name[i]);
-    i++;
-}
-printf("\n");
+struct book{
+    char name;
+    float page;
+    float price;
+};
+struct book b1 , b2 , b3;
+
+printf("Enter the name , page , price of book 1 :");
+scanf("%c %f %f ",&b1.name , &b1.page ,&b1.price);
+printf("Enter the  name , page and price of book 2 :");
+scanf("%c %f%f",&b2.name , &b2.page, &b2.price);
+printf("After we do that ");
+printf("%c %f%f",b1.name,b1.page,b1.price);
+printf("%c%f%f",b2.name , b2.page , b2.price);
 return 0;
 }
