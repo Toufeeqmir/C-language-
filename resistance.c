1e9ce0c932df13baad019dae721d26a097fd7628
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-10-24 11:46:57 +0530

    Add files via upload

---
+++ resistance.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+int main(){
+    
+    float resistance = 5.4;
+    float current = 5.3;
+    float potiential = current*resistance;
+    printf("the potiential is %f\n",potiential);
+    return 0;
+}
