78fefeec8ef6cf5c28b2ce70f4d6789f3d33db55
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-10-24 11:51:55 +0530

    Update mult rev.c

--- mult rev.c
+++ mult rev.c
@@ -1,17 +1,6 @@
 #include<stdio.h>
 int main(){
-   /* int a = 5;
-    printf("write the programe to print multiplicaton table in reverse order");
-    for(int i=10;i>=1;i--){
-        printf("%d *%d= %d\n",a , i , a*i);
-    }
-*/
-   /* int num;
-    printf("enter the value of num");
-    scanf("%d\n",&num);
-    for(int i = 10;i>=1;i--){
-        printf("%d*%d=%d\n",num , i , num*i);
-    }*/
+   
     int a , i;
     int sum = 0;
     printf("enter the value of a:\n");
