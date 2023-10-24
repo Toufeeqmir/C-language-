eb4ab8e07e00a7d28e5e67b9a07874f0cec17e05
Toufeeqmir <122149711+Toufeeqmir@users.noreply.github.com>
2023-10-24 13:38:33 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int day = 4;
+    switch (day)
+    {
+    case 1:
+        printf("monday \n");
+        break;
+    case 2:
+        printf("tuesday\n");
+        break;
+    case 3:
+        printf("wenesday\n");
+        break;
+    case 4:
+        printf("thursday\n");
+        break;
+    }
+
+    return 0;
+}
